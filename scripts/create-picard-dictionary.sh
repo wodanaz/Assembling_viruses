6b5d6c7d04dfe59b545c822f53c3fd95ff909a3d
John Bradley <johnbradley2008@gmail.com>
2021-02-05 13:16:28 -0500

    Removes hard coded picard.jar path

--- scripts/create-picard-dictionary.sh
+++ scripts/create-picard-dictionary.sh
@@ -10,5 +10,5 @@ set -e
 module load picard-tools/2.4.1-gcb01
 module load samtools/1.10-gcb01
 
-java -jar /nfs/software/helmod/apps/Core/picard-tools/2.4.1-gcb01/picard.jar  CreateSequenceDictionary R=MT246667.fasta O=MT246667.dict 
+java -jar $PICARD_TOOLS_HOME/picard.jar  CreateSequenceDictionary R=MT246667.fasta O=MT246667.dict
 samtools faidx MT246667.fasta

7a1b3352c9f401602b692280365dd4aa9fd5009f
John Bradley <johnbradley2008@gmail.com>
2021-02-05 11:54:25 -0500

    Adds a script to run sbatch array jobs
    Adds scripts/sbatch-array.sh that runs sbatch array jobs.
    This simplifies escape-variants-pipeline.sh so it is focused on high
    level processes.

--- scripts/escape-variants-pipeline.sh
+++ scripts/escape-variants-pipeline.sh
@@ -9,25 +9,6 @@
 # stop if a command fails (non-zero exit status)
 set -e
 
-MAX_ARRAY_JOBS=4
-
-
-# Run sbatch array job based on a file containing a list of filenames to process
-# Called like so:
-#  run_sbatch_array_job <sbatch_script_to_run> <filenames_file>
-# The FILENAMES_FILE environment variable is exported to be use by the sbatch_script_to_run
-run_sbatch_array_job () {
-  SCRIPTNAME=$1
-  # export FILENAMES_FILE for use by array job scripts
-  export FILENAMES_FILE=$2
-
-  NUM_READ_FILES=$(cat ${FILENAMES_FILE} | wc -l)
-  echo "Processing ${NUM_READ_FILES} files"
-  # create an array job for each file but only allow MAX_ARRAY_JOBS processes at a time
-  sbatch --wait --array=1-${NUM_READ_FILES}%${MAX_ARRAY_JOBS} $SCRIPTNAME
-}
-
-
 echo "Pipeline - Starting"
 
 
@@ -40,7 +21,7 @@ echo ""
 echo "Step 2 - Remove Nextera Adapters"
 # create the list of fastq.gz filenames to process
 ls *.fastq.gz > reads.list
-run_sbatch_array_job scripts/remove-nextera-adapters.sh reads.list
+./scripts/sbatch-array.sh scripts/remove-nextera-adapters.sh reads.list
 echo "Step 2 - Done"
 echo ""
 
@@ -48,7 +29,7 @@ echo ""
 echo "Step 3 - Map using BWA with the cleaned libraries"
 # create the list of trimmed reads to process
 ls *_trimmed.fq.gz > reads2.list
-run_sbatch_array_job scripts/map-bwa-cleaned-libs.sh reads2.list
+./scripts/sbatch-array.sh scripts/map-bwa-cleaned-libs.sh reads2.list
 echo "Step 3 - Done"
 echo ""
 
@@ -56,7 +37,7 @@ echo ""
 echo "Step 4 - Create BAM from SAM and make an index"
 # create the list of sam files to process
 ls *.sam > sams.list
-run_sbatch_array_job scripts/create-bma-from-sam.sh sams.list
+./scripts/sbatch-array.sh scripts/create-bma-from-sam.sh sams.list
 echo "Step 4 - Done"
 echo ""
 
---
+++ scripts/sbatch-array.sh
@@ -0,0 +1,21 @@
+#!/usr/bin/env bash
+# Runs sbatch array job.
+# Usage: sbatch-array.sh <SCRIPTNAME> <FILENAMES_FILE>
+# The SCRIPTNAME should be a sbatch array job script to run.
+# The FILENAMES_FILE is a file containing a list of files to process.
+# A job will be run for each line in FILENAMES_FILE.
+# The sbatch array job should read FILENAMES_FILE based on SLURM_ARRAY_TASK_ID to determine the filename to process.
+# eg. READFILE=$(awk NR==$SLURM_ARRAY_TASK_ID $FILENAMES_FILE)
+
+# Controls how many jobs are run at the same time.
+MAX_ARRAY_JOBS=4
+
+SCRIPTNAME=$1
+# export FILENAMES_FILE for use by array job scripts
+export FILENAMES_FILE=$2
+
+NUM_READ_FILES=$(cat ${FILENAMES_FILE} | wc -l)
+echo "Processing ${NUM_READ_FILES} files"
+# create an array job for each file but only allow MAX_ARRAY_JOBS processes at a time
+sbatch --wait --array=1-${NUM_READ_FILES}%${MAX_ARRAY_JOBS} $SCRIPTNAME
+

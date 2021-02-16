72c414b73df93d4648d7f228b0673e4645465dfd
Alejandro Berrio <alejo.berrio@duke.edu>
2021-02-16 17:23:44 -0500

    Update run-bcftools-consensus.sh

--- scripts/run-bcftools-consensus.sh
+++ scripts/run-bcftools-consensus.sh
@@ -15,4 +15,5 @@ root=`basename $VCFFILE .gatk.filt.vcf`
 bcftools view -Oz $VCFFILE > $VCFFILE.gz
 bcftools index $VCFFILE.gz
 bcftools norm -f $GENOME $VCFFILE.gz -Ob -o $root.norm.bcf
-bcftools consensus -m $root.merged.bed -f $GENOME  -p ${root}_ -s ${root} -H A $VCFFILE.gz | seqtk trimfq -b 25 -e 25 > $root.masked.fasta
+bcftools consensus -m $root.merged.bed -f $GENOME  -p ${root}_ -s ${root} -H A $VCFFILE.gz > $root.masked.fasta
+seqtk trimfq -b 25 -e 25  $root.masked.fasta > $root.cleaned.fasta

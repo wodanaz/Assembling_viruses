db33376530e7128b5ad28e107375654d8227136a
Alejandro Berrio <alejo.berrio@duke.edu>
2021-02-16 16:17:24 -0500

    Update create-coverage-table.sh

--- scripts/create-coverage-table.sh
+++ scripts/create-coverage-table.sh
@@ -7,12 +7,12 @@
 set -e
 
 module load bioawk
+GENOME_LENGTH=$((bioawk -c fastx '{ print length($seq)}' $GENOME))
 
-GENOME_LENGTH=bioawk -c fastx '{print ">" $name ORS length($seq)}' $GENOME
 
 for i in `cat depths.list`; do
     root=`basename $i .depth.bed`
-    percent=`awk '{ if ( $3 == 0 )  count++ } END { print 100 - ( count*100 / $GENOME_LENGTH  ) }' $i `
+    percent=`awk -vlength=$GENOME_LENGTH '{ if ( $3 == 0 )  count++ } END { print 100 - ( count*100 / length  ) }' $i `
     echo $root $percent >> table.tab
 done
 

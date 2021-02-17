8b8455c72b99adf20c7fed59e6aef721a89943ed
Alejandro Berrio <alejo.berrio@duke.edu>
2021-02-17 10:37:00 -0500

    Update create-coverage-table.sh
    Replaces the computation tool to obtain coverage info and makes table for coverage before and after running gatk pipeline

--- scripts/create-coverage-table.sh
+++ scripts/create-coverage-table.sh
@@ -6,17 +6,6 @@
 # stop if a command fails (non-zero exit status)
 set -e
 
-module load bioawk
-GENOME_LENGTH=$(bioawk -c fastx '{ print length($seq)}' $GENOME)
-
-
-for i in `cat depths.list`; do
-    root=`basename $i .depth.bed`
-    percent=`awk -v len=$GENOME_LENGTH '{ if ( $3 == 0 )  count++ } END { print 100 - ( count*100 / len  ) }' $i `
-    echo $root $percent >> table.tab
-done
-
-sort -k1,1 -V table.tab > table.sort.tab
 
 module load samtools
 
@@ -26,6 +15,13 @@ for i in `cat bams.list`; do
     echo $root $coverage >> coverage.tab
 done
 
-sort -k1,1 -V coverage.tab | sed 1i"sampleID\treference\tstartpos\tendpos\tnumreads\tcovbases\tcoverage\tmeandepth\tmeanbaseq\tmeanmapq" > coverage.sort.tab
+sort -k1,1 -V coverage.tab | sed 1i"sampleID\treference\tstartpos\tendpos\tnumreads\tcovbases\tcoverage\tmeandepth\tmeanbaseq\tmeanmapq" > coverage.raw.tab
 
 
+for i in `cat bqsrs.list`; do
+    root=`basename $i .bam`
+    coverage=`samtools coverage $i -H`
+    echo $root $coverage >> coverage.bqsr.tab
+done
+
+sort -k1,1 -V coverage.bqsr.tab | sed 1i"sampleID\treference\tstartpos\tendpos\tnumreads\tcovbases\tcoverage\tmeandepth\tmeanbaseq\tmeanmapq" > coverage.gatk.tab

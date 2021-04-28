d860cbbe8e839cbb980baf02be87b8feb67b9441
Alejandro Berrio Escobar <ab620@c1-01-3.genome.duke.edu>
2021-04-28 14:23:47 -0400

    Change order of merging files. Previous order was buggy

--- scripts/supermetadata-modify-titles.sh
+++ scripts/supermetadata-modify-titles.sh
@@ -17,14 +17,14 @@ sed -r 's/.bqsr//g' $EVDIR/coverage.gatk.tab | sed '1d' | awk '{print $1 "\t" $7
 
 join -j 1 <(sort $EVDIR/coverage.sort.tab) <( sort $DATETAB) | awk '{ print $1 "\t" $2 "\t" $3 }' > $EVDIR/metadata1.tab
 
-join -j 1 <(sort $EVDIR/metadata1.tab) <( sort $EVDIR/variants.tab ) | awk '{ print $1 "$" $2 "$" $3 "$" $4 }' > $EVDIR/supermetadata.tab
+join -j 1 <(sort $EVDIR/variants.tab) <( sort $EVDIR/metadata1.tab ) | awk '{ print $1 "$" $2 "$" $3 "$" $4 }' > $EVDIR/supermetadata.tab
 
 for i in `cat $EVDIR/supermetadata.tab`; do
     root=`echo ${i} | cut -d'$' -f 1`;
     rootA=`echo ${i} | cut -d'$' -f 2`;
     rootB=`echo ${i} | cut -d'$' -f 3`;
     rootC=`echo ${i} | cut -d'$' -f 4`;
-    result=${rootA/.*}
+    result=${rootB/.*}
     if [ "$result" -ge 90  ] ; then  # Here I use a filter to only modify fasta files with a coverage higher than 90%
         sed -r "s/_NC_045512/|${rootA}|${rootB}|${rootC}/g" $EVDIR/$root.cleaned.fasta > $EVDIR/$root.info.fasta ;
     fi ;

d4d4e8d781658685d2f77f8aca65e552cc7ba834
John Bradley <johnbradley2008@gmail.com>
2021-12-14 11:57:21 -0500

    Use 4 cores for trim_galore
    Speed up remove_adapters/trim_galore by specifying 4 cores.
    Fixes #76

--- scripts/remove-nextera-adapters.sh
+++ scripts/remove-nextera-adapters.sh
@@ -2,6 +2,7 @@
 # Remove Nextera Adapters
 # Required positional arguments:
 # - file to trim in fastq.gz format
+# Required TGCORES environment variable with the number of cores to use
 
 # stop if a command fails (non-zero exit status)
 set -e
@@ -9,7 +10,7 @@ set -e
 READFILE=$1
 OUTFILE=$2
 
-trim_galore --fastqc --nextera $READFILE
+trim_galore --cores $TGCORES --fastqc --nextera $READFILE
 
 BASEREAD=$(basename $READFILE)
 mv "${BASEREAD/.fastq.gz/_trimmed.fq.gz}" $OUTFILE
